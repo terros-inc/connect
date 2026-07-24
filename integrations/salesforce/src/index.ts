24aa6012e75c259749b8442618ac8f84d0ceab45
Jason Schofield <jason@terros.com>
2026-07-24 09:36:12 -0700

    sync

--- integrations/salesforce/src/index.ts
+++ integrations/salesforce/src/index.ts
@@ -40,12 +40,19 @@ export const handler = wrapConnectHandler<WebhookAccount>(async (input, client) 
 
   const response = await addLead({ clientId, clientSecret, url }, leadInput)
 
-  await client.call<AccountUpdateSuccess>('account/update', {
-    account: {
-      accountId: payload.data.id,
-      externalLeadId: response.id,
-    },
-  })
+  try {
+    await client.call<AccountUpdateSuccess>('account/update', {
+      account: {
+        accountId: payload.data.id,
+        externalLeadId: response.id,
+      },
+    })
+  } catch (err) {
+    const { message } = err instanceof Error ? err : { message: String(err) }
+    throw new Error(
+      `Created Salesforce lead ${response.id} for account ${payload.data.id} but failed to save externalLeadId back to the account (retrying this webhook will create a duplicate lead): ${message}`
+    )
+  }
 })
 
 function toSalesforceLead(account: WebhookAccount, leadType?: string): SalesforceLeadAddRequest | undefined {
@@ -63,11 +70,11 @@ function toSalesforceLead(account: WebhookAccount, leadType?: string): Salesforc
     City: location?.locality,
     State: location?.countrySubd,
     PostalCode: location?.postal1,
-    Latitude_Longitude__latitude__s: location?.latlng.latitude,
-    Latitude_Longitude__longitude__s: location?.latlng.longitude,
+    Latitude_Longitude__latitude__s: location?.latlng?.latitude,
+    Latitude_Longitude__longitude__s: location?.latlng?.longitude,
     Phone: resident.phone,
     Spotio_ID__c: account.data.id,
-    Lead_Type__c: leadType ?? 'Solar',
+    Lead_Type__c: leadType,
     Lead_Setter__c: account.data.owner ? `${account.data.owner.firstName} ${account.data.owner.lastName}` : undefined,
     Closer__c: account.data.closer ? `${account.data.closer.firstName} ${account.data.closer.lastName}` : undefined,
   }
