c5cd5114e4f4a525bb8a21ef91e9e2800b05a6f7
michelle <michelle@terros.com>
2026-08-31 11:08:12 -0700

    fix(connect): revert schema generator hook

--- cli/src/connect/generateSchemaJson.ts
+++ cli/src/connect/generateSchemaJson.ts
@@ -1,20 +1,6 @@
+import { ConnectConfig } from './configSchema'
 import { writeFileSync } from 'node:fs'
 import { join } from 'node:path'
-import { registerHooks } from 'node:module'
-
-registerHooks({
-  resolve(specifier, context, nextResolve) {
-    if (specifier.startsWith('.') && !specifier.endsWith('.ts')) {
-      try {
-        return nextResolve(`${specifier}.ts`, context)
-      } catch {}
-    }
-
-    return nextResolve(specifier, context)
-  },
-})
-
-const { ConnectConfig } = await import('./configSchema')
 
 const str = JSON.stringify(ConnectConfig.toJSONSchema(),null,2)
 
